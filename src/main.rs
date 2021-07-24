706a7c987dd733772bd9853daef6b09dd1ea9724
Cintra Guilherme <cinguilherme@gmail.com>
2021-07-24 11:11:53 -0300

    feat: 🎸 finalized

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 use num::Complex;
 use image::ColorType;
-use image::png::PngEncoder;
+use image::png::PNGEncoder;
 use std::fs::File;
 use std::env;
 
@@ -121,8 +121,8 @@ fn render(pixels: &mut [u8],
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
     let output = File::create(filename)?;
 
-    let encoder = PngEncoder::new(output);
-    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Rgba8);
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8));
 
     Ok(())
 }
